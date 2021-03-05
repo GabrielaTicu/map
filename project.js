d05ff7a4764e0c57d64ef6213d95a3099494530d
GabrielaTicu <78794186+GabrielaTicu@users.noreply.github.com>
2021-03-05 18:13:54 +0200

    Add files via upload

--- project.js
+++ project.js
@@ -1,1 +1,16 @@
+//set position for flags
+var h = 0;
+var ruTop = "";
+var worldMap = document.getElementById("worldMap");
+h  = worldMap.clientHeight;
+var flag = document.getElementById("ru");
+ruTop = 0.26*h;
+flag.style = "top: " + ruTop + "px";
 
+//onMouseOver for flags
+ru.onmouseover = function() {
+    ru.width = "10";
+}
+ru.onmouseout = function() {
+    ru.width = "3%";
+}
