45a05ba81aeb395d6e732a61dc2a6dc3a72e49f9
GabrielaTicu <78794186+GabrielaTicu@users.noreply.github.com>
2021-03-05 19:30:11 +0200

    Update project.js

--- project.js
+++ project.js
@@ -5,12 +5,12 @@ var worldMap = document.getElementById("worldMap");
 h  = worldMap.clientHeight;
 var flag = document.getElementById("ru");
 ruTop = 0.26*h;
-flag.style = "top: " + ruTop + "px";
+flag.style.top = ruTop + "px";
 
 //onmouseover for flags
 flag.onmouseover = function(){
-    flag.style = "border: thick solid lightblue; top: " + ruTop + "px";
+    flag.style.border = "thick solid lightblue";
 }
 flag.onmouseout = function(){
-    flag.style = "border: 0; top: " + ruTop + "px";
+    flag.style.border = "0";
 }
