fe6c2bd9e9e03e5953ec852ac701e1c66864eb00
GabrielaTicu <78794186+GabrielaTicu@users.noreply.github.com>
2021-03-14 13:01:19 +0200

    Add files via upload

--- project.js
+++ project.js
@@ -1,18 +1,34 @@
 //getting the map's height
 var h = 0;
-var worldMap = document.getElementById("worldMap");
-h  = worldMap.clientHeight;
+var map = document.getElementById("worldMap");
+h  = map.clientHeight;
 
 //set position for ru flag
 var ruTop = "";
-var flag = document.getElementById("ru");
+var ruFlag = document.getElementById("ru");
 ruTop = -0.76*h;
-flag.style.top = ruTop + "px";
+ruFlag.style.top = ruTop + "px";
 
 //set vertical position for us flag
+var usTop = "";
+var usFlag = document.getElementById("us");
+usTop = -0.61*h;
+usFlag.style.top = usTop + "px";
 
-//set vertical position for gb flag
+//set vertical position for uk flag
+var ukTop = "";
+var ukFlag = document.getElementById("uk");
+ukTop = -0.66*h;
+ukFlag.style.top = ukTop + "px";
 
 //set vertical position for br flag
+var brTop = "";
+var brFlag = document.getElementById("br");
+brTop = -0.33*h;
+brFlag.style.top = brTop + "px";
 
 //set vertical position for il flag
+var ilTop = "";
+var ilFlag = document.getElementById("il");
+ilTop = -0.54*h;
+ilFlag.style.top = ilTop + "px";
